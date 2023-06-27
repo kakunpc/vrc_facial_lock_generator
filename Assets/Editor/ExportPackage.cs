ad3656e20db0c6cdbee2519baf16f8f2066cff81
kakunpc <15257475+kakunpc@users.noreply.github.com>
2023-06-27 16:45:14 +0900

    export packageの追加

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class ExportPackage
+{
+    [MenuItem("kakunvr/ExportPackage")]
+    public static void CreatePackage()
+    {
+        AssetDatabase.ExportPackage(new[]
+        {
+            "Packages/com.kakunvr.facial-lock-generator",
+        }, "ExportedPackage.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
